ad8e588741318228621820b8ed4496969f98022d
Artem Sherbachuk <artemsherbachuk@gmail.com>
2015-08-14 22:13:46 +0300

    Add Model

---
+++ TableViewYalantisApp/ASDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  DataSource.swift
+//  TableViewYalantisApp
+//
+//  Created by typan on 8/14/15.
+//  Copyright (c) 2015 Artem Sherbachuk. All rights reserved.
+//
+
+import UIKit
+
+class DataSource: NSObject {
+   
+}
